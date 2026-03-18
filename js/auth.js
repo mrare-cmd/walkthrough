172680c37a94b165907614b3088441f1cd0c2c5b
mrare-cmd <mrareshide@greysteel.com>
2026-03-18 12:06:02 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,2 @@
 function isAuthed() { return true; }
 function logout() { window.location.href = 'index.html'; }
-
-if (document.getElementById('login-form')) {
-  window.location.href = 'app.html';
-}
-```
-
-Commit it, wait 30 seconds, then go straight to:
-```
-https://mrare-cmd.github.io/walkthrough/app.html
